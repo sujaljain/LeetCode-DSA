fb4ba5c10ca0511e455d4bf31c1ad1746e590ea9
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2025-03-21 13:21:18 +0530

    Time: 4 ms (95.78%), Space: 42.7 MB (94.81%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,15 +1,52 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder result = new StringBuilder();
-        String[] words = s.trim().split("\\s+");
+        char[] str = s.toCharArray();
+        int n = str.length;
 
-        for (int i = words.length-1; i>=0; i--){
-            result.append(words[i]);
-            if (i != 0){
-                result.append(" ");
-            }
+        // Step 1: Reverse the entire string character by character
+        reverse(str, 0, n - 1);
+
+        // Step 2: Reverse each word in-place
+        int i = 0, j = 0;
+        while (j < n) {
+            while (j < n && str[j] == ' ')
+                j++; // Skip Spaces
+
+            i = j; // Found the starting of word
+
+            while (j < n && str[j] != ' ')
+                j++; // Till the word ends
+
+            reverse(str, i, j - 1); // Reverse Each word now
         }
 
-        return result.toString();
+        // Step 3: Remove extra spaces & rebuild result
+        return cleanSpaces(str);
+    }
+
+    public void reverse(char[] str, int left, int right) {
+        while (left < right) {
+            char temp = str[left];
+            str[left++] = str[right];
+            str[right--] = temp;
+        }
+    }
+
+    public String cleanSpaces(char[] str) {
+        int i = 0, j = 0, n = str.length;
+
+        while (j < n) {
+            while (j < n && str[j] == ' ')
+                j++; // Skip (Leading) Spaces
+            while (j < n && str[j] != ' ')
+                str[i++] = str[j++]; // Copy word
+            while (j < n && str[j] == ' ')
+                j++; // Skip (Trailing) Spaces Again
+
+            if (j < n)
+                str[i++] = ' '; // Keep only one space
+        }
+
+        return new String(str, 0, i);
     }
 }
