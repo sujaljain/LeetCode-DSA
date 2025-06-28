9911b9d85cc84f813ead0075fe6c12adfd56e1ce
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2025-06-28 19:28:25 +0530

    Time: 0 ms (100%), Space: 41.3 MB (39.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,32 +1,39 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        // return rob_recursion(n - 1, nums, false);
+        if (n == 1)
+            return nums[0]; // Edge case
 
-        ArrayList<Integer> temp1 = new ArrayList<>();
-        ArrayList<Integer> temp2 = new ArrayList<>();
-
-        // If the array contains only 1 element
-        if (n == 1) return nums[0];
-
-
-        for (int i=0; i<n; i++){
-            if (i != 0) temp1.add(nums[i]);
-            if (i != n-1) temp2.add(nums[i]);
+        // Rob from index 0 to n-2 (exclude last)
+        int[] case1 = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            case1[i] = nums[i];
         }
 
-        return Math.max(space_optimised(temp1, temp1.size()), space_optimised(temp2, temp2.size()));
+        // Rob from index 1 to n-1 (exclude first)
+        int[] case2 = new int[n - 1];
+        for (int i = 1; i < n; i++) {
+            case2[i - 1] = nums[i];
+        }
+
+        return Math.max(space_optimised(case1), space_optimised(case2));
     }
 
-    private int space_optimised(ArrayList<Integer> nums, int n) {
-        int prev = nums.get(0);
+    private int space_optimised(int[] nums) {
+        int n = nums.length;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+
+        int prev = nums[0];
         int prev2 = 0;
 
         for (int i = 1; i < n; i++) {
-            int take = nums.get(i);
+            int take = nums[i];
             if (i > 1)
                 take += prev2;
-            int notTake = 0 + prev;
+            int notTake = prev;
             int curr = Math.max(take, notTake);
 
             prev2 = prev;
