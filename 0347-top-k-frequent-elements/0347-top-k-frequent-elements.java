66cb705c0726cdb1ce85ea187610cb08e231069a
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2025-07-01 23:09:39 +0530

    Time: 37 ms (5.12%), Space: 48.8 MB (53.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,37 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();    // For Storing array elements along with their Frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        List<Integer>[] bucket = new List[nums.length + 1];  // To store the element corresponding to the frequency
-
-        for (int el : nums) {
-            map.put(el, map.getOrDefault(el, 0) + 1);
+        // Storing all the freq of every particular num
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        // Iterating over all the keys of the map
-        for (int key : map.keySet()) {
-            int val = map.get(key);
+        // Storing the k most freq elements in a k sized array
+        int[] res = new int[k];
 
-            // Checking whether that list contains any value or not
-            if (bucket[val] == null) {
-                bucket[val] = new ArrayList<>();
-            }
+        for (int i = 0; i < k; i++) {
+            int maxFreq = -1;
+            int maxKey = -1;
 
-            bucket[val].add(key);
-        }
-
-        int[] ans = new int[k];
-        int idx = 0;
-
-        // Iterating in reverse over the bucket, to get the most frequency elements
-        for (int i = bucket.length - 1; i >= 0; i--) {
-            if (bucket[i] != null){
-                for (int j = 0; j<bucket[i].size() && idx < k; j++){
-                    ans[idx++] = bucket[i].get(j);
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > maxFreq) {
+                    maxFreq = entry.getValue();
+                    maxKey = entry.getKey();
                 }
             }
+            res[i] = maxKey;
+            map.remove(maxKey);
         }
 
-        return ans;
+        return res;
     }
 }
