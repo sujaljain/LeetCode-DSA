93cfbca3447209095a22d992980b638892e0ffd6
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2025-07-02 10:56:50 +0530

    Time: 6 ms (98.2%), Space: 48.3 MB (23.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,19 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> anagramMap = new HashMap<>();
 
-        for (String str : strs) {
-            // Sort the string to create a unique key
+        for (String str : strs){
             char[] charArray = str.toCharArray();
             Arrays.sort(charArray);
             String sortedKey = new String(charArray);
 
-            // Simplified way to add to the map
-            if (!anagramMap.containsKey(sortedKey)) {
-                anagramMap.put(sortedKey, new ArrayList<>());
+            if (!anagramMap.containsKey(sortedKey)){
+                anagramMap.put(sortedKey, new ArrayList<String>());
             }
+
             anagramMap.get(sortedKey).add(str);
         }
 
-        // Return the grouped anagrams
         return new ArrayList<>(anagramMap.values());
     }
 }
