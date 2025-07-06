317db0d609cfb8da4138a1d91021958367a19f76
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2025-07-06 13:59:23 +0530

    Time: 3 ms (50.72%), Space: 46 MB (21.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,27 +1,41 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
+        // Super Optimised Approach Of Greedy By Mr. Striver Kumar Desai
 
-        Boolean[] dp = new Boolean[n];
-
-        return solve(0, n, nums, dp);
-    }
-
-    private boolean solve(int idx, int n, int[] nums, Boolean[] dp) {
-        // Base Case
-        if (idx == n - 1)
-            return true;
-        if (idx >= n)
-            return false;
-
-        if (dp[idx] != null)    return dp[idx];
-
-        // Actual Processing
-        for (int i = nums[idx]; i > 0; i--) {
-            if (solve(idx + i, n, nums, dp))
-                return dp[idx + i] = true;
+        int maxIdx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxIdx)
+                return false;
+            maxIdx = Math.max(maxIdx, i + nums[i]);
         }
-
-        return dp[idx] = false;
+        return true;
     }
+
+    // DONE BY MYSELF: MR. SUJAL SA JAIN
+
+    // public boolean canJump(int[] nums) {
+    //     int n = nums.length;
+
+    //     Boolean[] dp = new Boolean[n];
+
+    //     return solve(0, n, nums, dp);
+    // }
+
+    // private boolean solve(int idx, int n, int[] nums, Boolean[] dp) {
+    //     // Base Case
+    //     if (idx == n - 1)
+    //         return true;
+    //     if (idx >= n)
+    //         return false;
+
+    //     if (dp[idx] != null)    return dp[idx];
+
+    //     // Actual Processing
+    //     for (int i = nums[idx]; i > 0; i--) {
+    //         if (solve(idx + i, n, nums, dp))
+    //             return dp[idx + i] = true;
+    //     }
+
+    //     return dp[idx] = false;
+    // }
 }
