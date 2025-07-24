9dd7bcf563040d0e81f4b156f91db6280cd6a140
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2025-07-24 21:23:23 +0530

    Time: 0 ms (100%), Space: 40.9 MB (83.96%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,6 +18,6 @@ class Solution {
         if (p == null && q == null)  return true;
         else if (p == null || q == null) return false;
 
-        else return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
