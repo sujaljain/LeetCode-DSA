9f85f0614d7c0d49f61bc45f8c6610fe358a9349
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2025-08-27 15:47:00 +0530

    Time: 5 ms (1.78%), Space: 54.4 MB (5.23%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,53 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = prices[0];
-        int maxProfit = 0;
+        int i=0;
+        int n = prices.length;
+        int transaction = 0;
+        int canBuy = 1;
+        int[][] dp = new int[n][2];
 
-        for (int i=1; i<prices.length; i++){
-            int profit = prices[i] - minPrice;
-            maxProfit = Math.max(maxProfit, maxProfit + profit);
-            minPrice = prices[i];
+        for (int[] idp : dp){
+            Arrays.fill(idp, -1);
+        }
+        return rec(i, canBuy, prices, n, dp);
+    }
+
+    private int rec(int i, int canBuy, int[] prices, int n, int[][] dp) {
+        if (i >= n) {
+            return 0;
         }
 
-        return maxProfit;
+        if (dp[i][canBuy] != -1){
+            return dp[i][canBuy];
+        }
+
+        int profit = Integer.MIN_VALUE;
+
+        if (canBuy==1) {
+            int buy = -prices[i] + rec(i, 0, prices, n, dp);
+            int skip = 0 + rec(i+1, 1, prices, n, dp);
+            profit = Math.max(buy, skip);
+        }
+
+        else {
+            int sell = prices[i] + rec(i + 1, 1, prices, n, dp);
+            int skip = 0 + rec(i + 1, 0, prices, n, dp);
+            profit = Math.max(sell, skip);
+        }
+
+        return dp[i][canBuy] = profit;
     }
+
+    // public int maxProfit(int[] prices) {
+    //     int minPrice = prices[0];
+    //     int maxProfit = 0;
+
+    //     for (int i=1; i<prices.length; i++){
+    //         int profit = prices[i] - minPrice;
+    //         maxProfit = Math.max(maxProfit, maxProfit + profit);
+    //         minPrice = prices[i];
+    //     }
+
+    //     return maxProfit;
+    // }
 }
